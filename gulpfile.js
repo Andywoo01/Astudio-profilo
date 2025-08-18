27935521aa1b818a313f5b9dd769eee55233550c
adesign412 <adesign412@gmail.com>
2025-08-17 20:58:13 -0700

    update
    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,29 @@
-const { watch } = require('gulp');
+const { src, dest, watch } = require('gulp');
+const minify = require('gulp-minify');
+const plumber = require('gulp-plumber');
+
+// Minify JS files
+function minifyJs() {
+    return src('javascript/**/*.js')
+        .pipe(plumber())
+        .pipe(minify({
+            ext: {
+                min: '.min.js'
+            },
+            noSource: true
+        }))
+        .pipe(dest('dist/js'));
+}
 
 // Simple watch task: watches all CSS and JS files in the project
 function watchFiles() {
-    watch(['**/*.css', '**/*.js'], function(cb) {
-        console.log('A CSS or JS file changed!');
+    watch(['**/*.css'], function(cb) {
+        console.log('A CSS file changed!');
         cb();
     });
+    watch(['javascript/**/*.js'], minifyJs);
 }
 
+exports.minify = minifyJs;
 exports.watch = watchFiles;
 exports.default = watchFiles;
